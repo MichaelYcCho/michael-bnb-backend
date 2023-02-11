e4abd60b3a1b124c1252759b28e3badc22af9a25
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-02-11 23:04:03 +0900

    chore: Poetry Version Issue

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 # exit on error
 set -o errexit
 
+echo "Installing the latest version of poetry..."
+export POETRY_HOME="$(pwd)/.poetry"
+curl -sSL https://install.python-poetry.org | python3 -
+export PATH="$POETRY_HOME/bin:$PATH"
+poetry --version
+
 poetry install
 
 python manage.py collectstatic --no-input
