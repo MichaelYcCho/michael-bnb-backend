377c514392c3ab046468e9bb4f4de984f617b655
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-03-01 22:56:01 +0900

    refactor: WishList Toggle Finish

--- wishlists/urls.py
+++ wishlists/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 
 from wishlists.views.view_v1_wish_list import WishlistToggleAPI
 
-urlpatterns = {
-    path("toggle/<int:room_id>", WishlistToggleAPI.as_view()),
-}
+urlpatterns = [
+    path("v1/toggle/<int:room_id>", WishlistToggleAPI.as_view()),
+]
--- wishlists/views/view_v1_wish_list.py
+++ wishlists/views/view_v1_wish_list.py
@@ -1,6 +1,7 @@
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
@@ -10,6 +11,8 @@ from wishlists.services.service_v1_wish_list import WishlistService
 
 
 class WishlistToggleAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
     @swagger_auto_schema(
         operation_summary="V1 WishList Toggle API",
         operation_description="찜 목록에 방을 추가 또는 제거",
